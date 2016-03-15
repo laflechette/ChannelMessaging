f679a4ab15b5057087ec63a0f1c8b0b75a56b5cb
Mathieu Larcher <larcher.mathieu@yahoo.fr>
2016-03-15 16:57:35 +0100

    Ajout de la carte, je ne suis pas arrivé à mettre les personnes sur la map car j'ai mon getLatitude et getLongitude qui sont égal à null, je pense qu'il y a un problème dans mon GPSActivity

--- app/src/main/java/mathieu/larcher/channelmessaging/Fragment/MessageFragment.java
+++ app/src/main/java/mathieu/larcher/channelmessaging/Fragment/MessageFragment.java
@@ -1,13 +1,18 @@
 package mathieu.larcher.channelmessaging.Fragment;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -16,8 +21,12 @@ import android.widget.ListView;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import mathieu.larcher.channelmessaging.app_gps.GPSActivity;
+import mathieu.larcher.channelmessaging.app_gps.MapActivity;
 import mathieu.larcher.channelmessaging.app_message.MasterMessage;
 import mathieu.larcher.channelmessaging.R;
 import mathieu.larcher.channelmessaging.adapter.MessageAdapter;
@@ -27,15 +36,19 @@ import mathieu.larcher.channelmessaging.network.onWSRequestListener;
 /**
  * Created by mathieularchet on 07/03/2016.
  */
-public class MessageFragment extends Fragment implements View.OnClickListener, onWSRequestListener {
+public class MessageFragment extends Fragment implements View.OnClickListener, onWSRequestListener, AdapterView.OnItemClickListener {
 
     private static final int REQUEST_MESSAGE = 1;
     private static final int REQUEST_SEND_MESSAGE = 0;
+    protected Location mCurrentLocation;
     private String token;
     private HashMap<String, String> mapParams = new HashMap<>();
     private Handler handler;
     public int channelID;
     private SharedPreferences settings;
+    protected ArrayList<Double> messagesList;
+
+
     ListView lst_view_fragment;
     Button btnSend;
     EditText txt_field;
@@ -50,6 +63,7 @@ public class MessageFragment extends Fragment implements View.OnClickListener, o
         channelID = getActivity().getIntent().getIntExtra("channelsID", 1);
 
         lst_view_fragment = (ListView) v.findViewById(R.id.listView);
+        lst_view_fragment.setOnItemClickListener(this);
 
         txt_field = (EditText) v.findViewById(R.id.txt_view_message);
         btnSend = (Button) v.findViewById(R.id.btn_send);
@@ -136,4 +150,44 @@ public class MessageFragment extends Fragment implements View.OnClickListener, o
 
         txt_field.setText("");
     }
-}
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+        List<String> listItems = new ArrayList<String>();
+        listItems.add("Ajouter un ami");
+        listItems.add("Voir sur la carte");
+
+        if ( ((GPSActivity) getActivity()).getmCurrentLocation() != null){
+
+            String latitude = String.valueOf(((GPSActivity)getActivity()).mCurrentLocation.getLatitude());
+            String longitude = String.valueOf(((GPSActivity)getActivity()).mCurrentLocation.getLongitude());
+
+            double latitude_double = Double.valueOf(latitude);
+            double longitude_double = Double.valueOf(longitude);
+
+            mapParams.put("latitude", latitude);
+            mapParams.put("longitude", longitude);
+        }
+
+            final CharSequence[] arr = listItems.toArray(new CharSequence[listItems.size()]);
+            new AlertDialog.Builder(getActivity())
+                    .setIcon(android.R.drawable.ic_dialog_alert)//drawable de l'icone à gauche du titre
+                    .setTitle("Choisissez une option")//Titre de l'alert dialog
+                    .setItems(arr, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {//which = la position de l'item appuyé
+                            if (which == 0) {
+                                //Do some stuff (1st item touched)
+                            } else {
+                                //Do some over stuff (2nd item touched)
+                                Intent myIntent = new Intent(getContext(), MapActivity.class);
+                                myIntent.putExtra("username", mapParams.get("username")); //Optional parameters
+                                myIntent.putExtra("latitude", mapParams.get("latitude")); //Optional parameters
+                                myIntent.putExtra("longitude", mapParams.get("longitude")); //Optional parameters
+                                startActivity(myIntent);
+                            }
+                        }
+                    })//items de l'alert dialog
+                    .show();
+        }
+    }
